9cfe74ac0e917a7d56b69afa4c523f50386204e1
Edison Furusato <edison.oda@hotmail.com>
2024-05-06 15:33:29 -0300

    Fixed enemy props
    Changed variable names from uppercase to lowercase

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 class_name Enemy
 
-@export var SPEED: float = 0.5
-@export var ACCELERATION: float = 1.0
-@export var PLAYER_DISTANCE: float = 5.0
-@export var HEALTH: float = 3.0
-@export var WEIGHT: float = 5.0
-@export var EXP: float = 10.0
+@export var speed: float = 0.5
+@export var acceleration: float = 1.0
+@export var player_distance: float = 5.0
+@export var health: float = 3.0
+@export var weight: float = 5.0
+@export var exp: float = 10.0
 
 @onready var player = %Player
 
@@ -15,10 +15,10 @@ var velocity: Vector2 = Vector2(0.0, 0.0)
 var knockback_staggered: bool = false
 
 func get_weight() -> float:
-	return WEIGHT
+	return weight
 
 func _physics_process(delta):
-	if velocity.length() < SPEED:
+	if velocity.length() < speed:
 		knockback_staggered = false
 	
 	move(delta)
--- scripts/enemies/wasp.gd
+++ scripts/enemies/wasp.gd
@@ -3,16 +3,16 @@ extends Enemy
 @onready var sprite = $AnimatedSprite2D
 
 func move(delta):
-	if position.distance_to(player.position) < PLAYER_DISTANCE:
+	if position.distance_to(player.position) < player_distance:
 		velocity = Vector2(0.0, 0.0)
 		return
 	
 	var direction = position.direction_to(player.position)
 	
-	velocity.x += direction.x * delta * ACCELERATION
-	velocity.y += direction.y * delta * ACCELERATION
+	velocity.x += direction.x * delta * acceleration
+	velocity.y += direction.y * delta * acceleration
 	
-	if velocity.length() > SPEED and not knockback_staggered:
-		velocity = velocity.limit_length(SPEED)
+	if velocity.length() > speed and not knockback_staggered:
+		velocity = velocity.limit_length(speed)
 	
 	sprite.flip_h = player.position.x < position.x
